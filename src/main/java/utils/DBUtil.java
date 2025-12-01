491a3dea906e119da4ac16dcd966aaf1cfde9331
deemceegit <doanminhcuong0702@gmail.com>
2025-12-01 15:57:14 +0700

    DBUtil Nuage

--- src/main/java/utils/DBUtil.java
+++ src/main/java/utils/DBUtil.java
@@ -5,18 +5,26 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/programmize";
-    private static final String USER = "root";
-    private static final String PASSWORD = "123456";
 
-    public static Connection getConnection() {
-        Connection conn = null;
+    private static final String HOST = "programmize-doanminhcuong0702-0a97.g.aivencloud.com";
+    private static final String PORT = "11985";
+    private static final String DB_NAME = "programmize";
+    private static final String USER = "avnadmin";
+    private static final String PASSWORD = "AVNS_Qs2V0mmVj8lWkpgzDhJ";
+    private static final String URL = "jdbc:mysql://" + HOST + ":" + PORT + "/" + DB_NAME + "?sslMode=REQUIRED" + "&useSSL=true" + "&allowPublicKeyRetrieval=true";
+
+    static {
         try {
+            // Nạp Driver MySQL (Đảm bảo project của bạn đã có thư viện mysql-connector-java)
             Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (Exception e) {
+        } catch (ClassNotFoundException e) {
+            System.err.println("Không tìm thấy Driver MySQL! Hãy kiểm tra lại thư viện.");
             e.printStackTrace();
         }
-        return conn;
+    }
+
+    public static Connection getConnection() throws SQLException {
+        System.out.println("Connecting to: " + URL);
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
