6d5156439170b59fb467ee088b9ecc652293bdb6
Asus <trunghiennguyen71@gmail.com>
2025-12-09 20:44:23 +0700

    remove homeDAO

--- src/main/java/servlet/HomeServlet.java
+++ src/main/java/servlet/HomeServlet.java
@@ -1,6 +1,6 @@
 package servlet;
 
-import dao.HomeDAO;
+import dao.CourseDAO;
 import model.Course;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -17,10 +17,10 @@ public class HomeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HomeDAO homeDAO = new HomeDAO();
+        CourseDAO courseDAO = new CourseDAO();
 
         // 1. Lấy danh sách khóa học
-        List<Course> highlightedCourses = homeDAO.getHighlightedCourses();
+        List<Course> highlightedCourses = courseDAO.getHighlightedCourses();
 
         // 2. Đẩy dữ liệu vào request attribute
         request.setAttribute("highlightedCourses", highlightedCourses);
