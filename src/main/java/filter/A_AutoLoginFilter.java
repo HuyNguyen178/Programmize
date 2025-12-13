7d0be63932c8a2aa16dd9e96b460340bdb8456bd
unknown <vuthai010705@gmail.com>
2025-12-13 10:21:19 +0700

    update Authentication va Authorization filter

--- src/main/java/filter/A_AutoLoginFilter.java
+++ src/main/java/filter/A_AutoLoginFilter.java
@@ -11,7 +11,7 @@ import model.User;
 import java.io.IOException;
 
 @WebFilter("/*")
-public class AutoLoginFilter implements Filter {
+public class A_AutoLoginFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
