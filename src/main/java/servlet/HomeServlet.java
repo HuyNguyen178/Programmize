9cb47bb22c10fe7089e6d8dbbe32d289266a7e26
HuiNek <huyng.23bi14194@usth.edu.vn>
2025-12-30 10:16:07 +0700

    change home back

--- src/main/java/servlet/HomeServlet.java
+++ src/main/java/servlet/HomeServlet.java
@@ -14,7 +14,7 @@ import java.util.List;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
-@WebServlet("/")
+@WebServlet("/home")
 public class HomeServlet extends HttpServlet {
     private final Logger logger = LogManager.getLogger(HomeServlet.class);
 
